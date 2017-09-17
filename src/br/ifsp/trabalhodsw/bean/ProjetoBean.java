56a3666aab0a6c5789b8683d6eecc0d4afc23538
Lucas Marchi <lucas-marchi@hotmail.com>
2017-09-17 20:59:25 -0300

    Testando o GitKraken
    Excluir o arquivo criado, após os testes

--- src/br/ifsp/trabalhodsw/bean/ProjetoBean.java
+++ src/br/ifsp/trabalhodsw/bean/ProjetoBean.java
@@ -19,8 +19,6 @@ public class ProjetoBean {
 	private Projeto projeto = new Projeto();
 	private List<Voluntario> voluntarios = new ArrayList<Voluntario>();
 	private String cpf;
-	
-	
 
 	public String getCpf() {
 		return cpf;
@@ -37,7 +35,7 @@ public class ProjetoBean {
 	public void setProjeto(Projeto projeto) {
 		this.projeto = projeto;
 	}
-	
+
 	public List<Voluntario> getVoluntarios() {
 		return voluntarios;
 	}
@@ -50,38 +48,47 @@ public class ProjetoBean {
 
 		if (projeto.getNome().isEmpty()) {
 			FacesContext.getCurrentInstance().addMessage("messages", new FacesMessage("Projeto deve conter um nome"));
+			projeto = new Projeto();
 			return "cadastro_projeto";
 		}
 
 		if (projeto.getId() != null && projeto.getId().intValue() > 0) {
 			new Repository<Projeto>(Projeto.class).atualiza(this.projeto);
+			projeto = new Projeto();
 			return "lista_projeto?faces-redirect=true";
 		} else {
 			new Repository<Projeto>(Projeto.class).adiciona(this.projeto);
+			projeto = new Projeto();
 			return "lista_projeto?faces-redirect=true";
 		}
-		
 
 	}
 
 	public List<Projeto> getListaProjetos() {
 		return new Repository<Projeto>(Projeto.class).listaTodos();
 	}
-	
+
 	public String adicionarVoluntario(Projeto projeto) {
 		this.projeto = projeto;
 		return "adicionar_voluntarios";
 	}
-	
+
 	public void gravarVoluntario(Voluntario voluntario) {
 		projeto.getVoluntarios().add(voluntario);
 		new Repository<Projeto>(Projeto.class).atualiza(projeto);
 		FacesContext.getCurrentInstance().addMessage("messages", new FacesMessage("Adicionado com sucesso!!"));
 	}
-	
+
 	public void pesquisaeVoluntario() {
 		voluntarios = new ArrayList<>();
-		voluntarios.add( (Voluntario) new Repository<Voluntario>(Voluntario.class).buscaPorCpf(cpf));
+		voluntarios.add((Voluntario) new Repository<Voluntario>(Voluntario.class).buscaPorCpf(cpf));
+	}
+
+	public String removerProjeto() {
+		projeto = new Repository<Projeto>(Projeto.class).buscaPorId(projeto.getId());
+		new Repository<Projeto>(Projeto.class).remove(projeto);
+		return null;
 	}
 	
+
 }
