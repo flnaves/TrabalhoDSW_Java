1e765f97228a8590e340cbe7b83a0905c5c38281
Lucas Marchi <lucas-marchi@hotmail.com>
2017-09-17 13:40:51 -0300

    Adicionar Voluntario

--- src/br/ifsp/trabalhodsw/bean/ProjetoBean.java
+++ src/br/ifsp/trabalhodsw/bean/ProjetoBean.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
 import br.ifsp.trabalhodsw.entities.Projeto;
 import br.ifsp.trabalhodsw.repository.Repository;
 
 @ManagedBean
+@SessionScoped
 public class ProjetoBean {
 
 	private Projeto projeto = new Projeto();
@@ -22,22 +24,31 @@ public class ProjetoBean {
 		this.projeto = projeto;
 	}
 
-	public void gravar() {
+	public String gravar() {
 
 		if (projeto.getNome().isEmpty()) {
 			FacesContext.getCurrentInstance().addMessage("nome", new FacesMessage("Projeto deve conter um nome"));
+			return "cadastro_projeto";
 		}
 
 		if (projeto.getId() != null && projeto.getId().intValue() > 0) {
 			new Repository<Projeto>(Projeto.class).atualiza(this.projeto);
+			return "lista_projeto?faces-redirect=true";
 		} else {
 			new Repository<Projeto>(Projeto.class).adiciona(this.projeto);
+			return "lista_projeto?faces-redirect=true";
 		}
+		
 
 	}
 
 	public List<Projeto> getListaProjetos() {
 		return new Repository<Projeto>(Projeto.class).listaTodos();
+	}
+	
+	public String adicionarVoluntario(Projeto projeto) {
+		this.projeto = projeto;
+		return "adicionar_voluntarios";
 	}
 
 }
